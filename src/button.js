00346d0f5e7dbcd161c576d39fc738487fe1e7a2
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-05-08 14:35:47 -0500

    lcoal wip

--- src/button.js
+++ src/button.js
@@ -1,11 +1,10 @@
-// take a str, the button label and return a element
-/**
- *
- * @param {string} buttonName
- * @returns {Element}
- */
 const makeButton = buttonName => {
-  return `Button: ${buttonName}`;
+  const buttonLabel = `Button: ${buttonName}`;
+
+  const button = document.createElement('button');
+  button.innerText = buttonLabel;
+
+  return button;
 };
 
 module.exports = makeButton;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  mode: "none"
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const webpackMerge = require('webpack-merge');
+
+module.exports = ({mode, presets} = {mode: 'production', presets: []}) => {
+    console.log(mode);
+    return webpackMerge({
+        mode,
+        output: {
+            filename: "bundle.js"
+        },
+        plugins: [
+            new HtmlWebpackPlugin(), new webpack.ProgressPlugin()
+        ]
+    },
+        modeConfig(mode));
 };
