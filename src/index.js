3f88ad974896b088b1891e8f5202d166b7dc3427
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-05-09 10:35:44 -0500

    local wip

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import nav from "./nav";
-import { footer } from "./footer";
+const getFooter = () => import('./footer');
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import makeImage from "./image";
@@ -12,5 +12,10 @@ const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 
 document.body.appendChild(button);
+button.addEventListener('click', e => {
+    import('./footer').then(footerModule => {
+        document.body.appendChild(footerModule.footer);
+    });
+});
+
 document.body.appendChild(image);
-document.body.appendChild(footer);
