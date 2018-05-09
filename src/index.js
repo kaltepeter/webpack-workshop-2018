35c4ccff57a4bcf5929794cdab9b7643d6cc9c4a
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-05-09 11:32:59 -0500

    local wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import nav from "./nav";
+const getGSAP = () => import('gsap');
 // import { footer } from "./footer";
 const getFooter = () => import("./footer");
+const getLodashUniq = () => import('lodash-es/uniq');
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import makeImage from "./image";
@@ -18,6 +20,14 @@ button.addEventListener("click", e => {
     getFooter().then(footerModule => {
         document.body.appendChild(footerModule.footer);
     });
+
+    getLodashUniq().then(uniq => {
+        console.log(uniq);
+    });
+
+    getGSAP().then(gsap => {
+        console.log(gsap);
+    })
 });
 
 
